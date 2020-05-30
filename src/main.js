27700bbb88a69ec7276049cd51db97c747c4b32c
peterorbb <orbitalist@gmail.com>
2020-05-30 20:13:56 +0300

    Setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({ render: h => h(App), store, router }).$mount('#app');
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+// import Index from '../components/Index';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'hash',
+  routes: [
+    // {
+    //   component: Index,
+    //   name: 'Index',
+    //   path: '/',
+    // },
+  ],
+});
